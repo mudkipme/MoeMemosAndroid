d9fbf5aa85f4005071ff3da14d0e841ef5458544
Mudkip <i@mudkip.me>
2022-11-23 23:46:57 +0800

    feat: take photos with camera

---
+++ app/src/main/java/me/mudkip/moememos/MoeMemosFileProvider.kt
@@ -0,0 +1,21 @@
+package me.mudkip.moememos
+
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.FileProvider
+import java.io.File
+
+class MoeMemosFileProvider: FileProvider(
+    R.xml.file_paths
+) {
+    companion object {
+        fun getImageUri(context: Context): Uri {
+            val directory = File(context.cacheDir, "images")
+            directory.mkdirs()
+
+            val file = File.createTempFile("capture_picture_", ".jpg", directory)
+            val authority = context.packageName + ".fileprovider"
+            return getUriForFile(context, authority, file)
+        }
+    }
+}
