2ab50b2f2feaa7ad8374dba7234f2aa09bfa1436
Mudkip <i@mudkip.me>
2023-09-19 00:31:20 +0800

    fix: crash in release mode

--- app/src/main/java/me/mudkip/moememos/data/module/NetworkModule.kt
+++ app/src/main/java/me/mudkip/moememos/data/module/NetworkModule.kt
@@ -1,17 +1,15 @@
 package me.mudkip.moememos.data.module
 
 import android.content.Context
+import com.franmontiel.persistentcookiejar.PersistentCookieJar
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import net.gotev.cookiestore.SharedPreferencesCookieStore
-import net.gotev.cookiestore.WebKitSyncCookieManager
-import net.gotev.cookiestore.okhttp.JavaNetCookieJar
 import okhttp3.OkHttpClient
-import java.net.CookieManager
-import java.net.CookiePolicy
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,15 +17,9 @@ object NetworkModule {
 
     @Provides
     fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
-        val cookieManager = WebKitSyncCookieManager(
-            store = SharedPreferencesCookieStore(context, "cookies"),
-            cookiePolicy = CookiePolicy.ACCEPT_ALL
-        )
-
-        CookieManager.setDefault(cookieManager)
-
+        val cookieJar = PersistentCookieJar(SetCookieCache(), SharedPrefsCookiePersistor(context))
         return OkHttpClient.Builder()
-            .cookieJar(JavaNetCookieJar(cookieManager))
+            .cookieJar(cookieJar)
             .build()
     }
 }
