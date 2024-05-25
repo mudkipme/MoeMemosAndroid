8e29a207a3bb0389975b3d81a8fa6f75ded4f466
mudkipme <i@mudkip.me>
2024-05-25 14:21:34 +0800

    wip: memos v1 api

--- app/src/main/java/me/mudkip/moememos/data/api/Memos.kt
+++ app/src/main/java/me/mudkip/moememos/data/api/Memos.kt
@@ -6,24 +6,32 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = false)
 enum class MemosRole {
+    @field:Json(name = "ROLE_UNSPECIFIED")
+    ROLE_UNSPECIFIED,
     @field:Json(name = "HOST")
     HOST,
+    @field:Json(name = "ADMIN")
+    ADMIN,
     @field:Json(name = "USER")
     USER
 }
 
 @JsonClass(generateAdapter = false)
 enum class MemosVisibility {
-    @field:Json(name = "PUBLIC")
-    PUBLIC,
+    @field:Json(name = "VISIBILITY_UNSPECIFIED")
+    VISIBILITY_UNSPECIFIED,
+    @field:Json(name = "PRIVATE")
+    PRIVATE,
     @field:Json(name = "PROTECTED")
     PROTECTED,
-    @field:Json(name = "PRIVATE")
-    PRIVATE
+    @field:Json(name = "PUBLIC")
+    PUBLIC
 }
 
 @JsonClass(generateAdapter = false)
 enum class MemosRowStatus {
+    @field:Json(name = "ROW_STATUS_UNSPECIFIED")
+    ROW_STATUS_UNSPECIFIED,
     @field:Json(name = "NORMAL")
     NORMAL,
     @field:Json(name = "ARCHIVED")
--- app/src/main/java/me/mudkip/moememos/data/api/MemosV1Api.kt
+++ app/src/main/java/me/mudkip/moememos/data/api/MemosV1Api.kt
@@ -1,5 +1,86 @@
 package me.mudkip.moememos.data.api
 
-interface MemosV1Api {
+import androidx.annotation.Keep
+import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+import java.util.Date
 
+interface MemosV1Api {
+    @POST("/api/v1/auth/signin")
+    suspend fun signIn(@Body body: MemosV1SignInRequest): ApiResponse<MemosV1User>
+
+    @POST("api/v1/auth/signout")
+    suspend fun signOut(): ApiResponse<Unit>
+
+    @POST("api/v1/auth/status")
+    suspend fun authStatus(): ApiResponse<MemosV1User>
+
+    @GET("api/v1/memos")
+    suspend fun listMemos(
+        @Query("pageSize") pageSize: Int,
+        @Query("pageToken") pageToken: String,
+        @Query("filter") filter: String
+    ): ApiResponse<List<MemosV1Memo>>
+
+    @POST("api/v1/memos")
+    suspend fun createMemo(@Body body: MemosV1CreateMemoRequest): ApiResponse<MemosV1Memo>
 }
+
+@Keep
+data class MemosV1User(
+    val name: String,
+    val id: Int,
+    val role: MemosRole,
+    val username: String,
+    val email: String,
+    val nickname: String,
+    val avatarUrl: String,
+    val description: String,
+    val rowStatus: MemosRowStatus,
+    val createTime: Date,
+    val updateTime: Date
+)
+
+@Keep
+data class MemosV1SignInRequest(
+    val username: String,
+    val password: String,
+    val neverExpire: Boolean
+)
+
+@Keep
+data class MemosV1CreateMemoRequest(
+    val content: String,
+    val visibility: MemosVisibility?
+)
+
+@Keep
+data class MemosV1Memo(
+    val name: String,
+    val uid: String,
+    val rowStatus: MemosRowStatus,
+    val creator: String,
+    val createTime: Date,
+    val updateTime: Date,
+    val displayTime: Date,
+    val content: String,
+    val visibility: MemosVisibility,
+    val tags: List<String>,
+    val pinned: Boolean,
+    val resources: List<MemosV1Resource>
+)
+
+@Keep
+data class MemosV1Resource(
+    val name: String,
+    val uid: String,
+    val createTime: Date,
+    val filename: String,
+    val externalLink: String,
+    val type: String,
+    val size: Int,
+    val memo: String?
+)
