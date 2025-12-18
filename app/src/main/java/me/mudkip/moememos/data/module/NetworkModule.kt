55094186eabe9a410a1bebc4a2102d8a7483659e
Cerne1Lin <hsl_qmail@qq.com>
2025-12-19 00:39:21 +0800

    移除TLS证书验证

--- app/src/main/java/me/mudkip/moememos/data/module/NetworkModule.kt
+++ app/src/main/java/me/mudkip/moememos/data/module/NetworkModule.kt
@@ -15,10 +15,25 @@ object NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
-        val cookieManager = CookieManager()
-        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
-        return OkHttpClient.Builder()
-            .cookieJar(JavaNetCookieJar(cookieManager))
-            .build()
+    val cookieManager = CookieManager()
+    cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
+    
+    // 信任所有 SSL 证书
+    val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+        override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+    })
+    
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, trustAllCerts, SecureRandom())
+    val sslSocketFactory = sslContext.socketFactory
+    
+    return OkHttpClient.Builder()
+        .cookieJar(JavaNetCookieJar(cookieManager))
+        // 添加 SSL 证书信任配置
+        .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        .hostnameVerifier { _, _ -> true } // 忽略主机名验证
+        .build()
     }
 }
